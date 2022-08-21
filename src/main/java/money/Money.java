8ad85e6c4a4f51a34f3cce9c7976b0f7e37fc826
haruto830 <haruto8300@gmail.com>
2022-08-21 16:56:08 +0900

    第9章 歩幅の調整
    - サブクラスの差異を呼びだし側に移動することで、共通化しやすいよう寄せていった。
    - 同じに内容になったコンストラクタを親クラスに引き上げた

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -2,15 +2,25 @@ package money;
 
 abstract class Money {
     protected int amount;
+    protected String currency;
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
 
     abstract Money times(int multiplier);
 
+    public String currency() {
+        return currency;
+    }
+
     public static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount,"USD");
     }
 
     public static Franc franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount,"CHF");
     }
 
     @Override
